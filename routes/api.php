a599f5c5c90f24aefd45c6894db876975affb283
dimuthu sandaruwan jayalath <d.jayalath@stylesoftware.co>
2018-06-06 14:17:35 +0530

    CRUD for category

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 Route::model('/question', 'App\Model\Question');
+Route::model('/category', 'App\Model\Category');
 
 
 Route::apiResource('/question', 'QuestionController');
+Route::apiResource('/category', 'CategoryController');
